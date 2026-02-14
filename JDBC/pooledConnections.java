95ac6ee44af246b04e298662f4923ab3d59c1345
gopika jitesh <gopika.jitesh2004@gmail.com>
2026-02-14 17:13:20 +0530

    Refactor connection handling to use DataSource
    Updated connection handling to use DataSource for pooled connections and added example for closing connections properly.

--- JDBC/pooledConnections.java
+++ JDBC/pooledConnections.java
@@ -57,3 +57,22 @@ public class ConnectionPoolingBean implements SessionBean {
     private DataSource ds = null;
     private Context ctx = null;
 }
+
+
+
+Use a DataSource object rather than the DriverManager class to get a connection. 
+    ds is a DataSource object implemented and deployed so that it will create pooled connections and username and password are variables that represent the credentials of the user that has access to the database:
+
+Connection con = ds.getConnection(username, password);
+Use a finally statement to close a pooled connection. The following finally block would appear after the try/catch block that applies to the code in which the pooled connection was used:
+
+try {
+    Connection con = ds.getConnection(username, password);
+    // ... code to use the pooled
+    // connection con
+} catch (Exception ex {
+    // ... code to handle exceptions
+} finally {
+    if (con != null) con.close();
+}
+Otherwise, an application using a pooled connection is identical to an application using a regular connection.
