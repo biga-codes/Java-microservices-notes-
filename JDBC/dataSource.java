e6e528e633fcee4f93d0a107e250f4b7dc8404dc
gopika jitesh <gopika.jitesh2004@gmail.com>
2026-02-14 17:03:35 +0530

    Enhance documentation for DataSource connection
    Added explanation for DataSource usage and connection retrieval.

--- JDBC/dataSource.java
+++ JDBC/dataSource.java
@@ -6,3 +6,12 @@ ds.setDescription("Customer accounts database for billing");
 DataSource.getConnection to get a connection to the company's database, CUSTOMER_ACCOUNTS
   The variable ds now represents the database CUSTOMER_ACCOUNTS installed on the server.
   Any connection produced by the BasicDataSource object ds will be a connection to the database CUSTOMER_ACCOUNTS.
+
+  After a basic DataSource implementation is deployed by a system administrator,
+  it is ready for a programmer to use. This means that a programmer can give the logical data source name that was 
+  bound to an instance of a DataSource class, 
+  and the JNDI naming service will return an instance of that DataSource class
+
+Connection con = ds.getConnection("fernanda","brewed");
+
+The getConnection method requires only the user name and password because the variable ds has the rest of the information
